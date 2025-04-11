63b9259bfaedd12fd1876e084a8fd002caac85c4
Ahmad Idris <ahmidris2030@gmail.com>
2025-04-11 17:33:52 +0300

    New Change

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*===================Typing Animation===================== */
+var typed = new Typed(".typing",{
+ strings:["","Web Developer", "Web Designer", "Graphic Designer",
+     "UI/UX Designer", "YouTuber"],
+ typeSpeed:100,
+ backSpeed:60,
+ loop:true
+})
